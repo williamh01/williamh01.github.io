6577dd89d966657515bb44249ca252cd10c6a7dc
williamh01 <78118353+williamh01@users.noreply.github.com>
2021-09-15 21:35:08 -0400

    Update main.js

--- main.js
+++ main.js
@@ -31,31 +31,3 @@ window.onscroll = function () {
     prevScrollpos = currentScrollPos;
 };
 
-
-document.querySelector('.contactForm').addEventListener('submit', submitForm);
-function submitForm(e) {
-    e.preventDefault();
-
-    //   Get input Values
-    let name = document.getElementById("name_input").value;
-    let email = document.getElementById("email_input").value;
-    let message = document.getElementById("message_input").value;
-    console.log(name, email, message);
-    document.querySelector('.contactForm').reset();
-
-    sendEmail(name, email, message);
-}
-
-
-function sendEmail(name, email, message) {
-    Email.send({
-        Host: "smtp.gmail.com",
-        Username: "williamhsia28@gmail.com",
-        Password: "dcjobgbjibpanmwk",
-        To: "williamhsia28@gmail.com",
-        From: "williamhsia28@gmail.com",
-        Subject: `${name} sent you a message`,
-        Body: `Name: ${name} <br/> Email: ${email} <br/> Message: ${message}`,
-    }).then((message) => alert("mail sent successfully"));
-
-}
